037b6880b67eb85fe9b3572ff08113bf61bedf4c
M. Ahsan Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2022-04-15 23:02:26 +0500

    For Brand version 4

---
+++ brand-v4/footer.php
@@ -0,0 +1,22 @@
+	<section class="contact-us-section">
+			
+	</section>
+	
+
+	<footer>
+			
+	</footer>
+
+
+
+<!-- <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script> -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery.fancybox.min.js"></script>
+<script src="js/popper-min.js"></script>
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/custom.js"></script> 
+
+
+</body>
+</html>
---
+++ brand-v4/index.php
@@ -0,0 +1,7 @@
+<?php include 'header.php';?>
+
+	<section class="">
+		
+	</section>
+
+<?php include 'footer.php';?>
