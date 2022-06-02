abfcfd8d084b7fbbfa5a26b01667d6ecdf00d791
M. Ahsan Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2022-06-03 04:02:56 +0500

    Add files via upload

---
+++ starter-volume-2/components/header.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>TITLE HERE </title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
+    <link rel="stylesheet" href="../assets/css/aos.css">
+    <link rel="stylesheet" href="../assets/css/animate.min.css" />
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../assets/css/video.css" />
+    <link rel="stylesheet" href="../assets/css/jquery.fancybox.min.css" />
+    <link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../assets/css/owl.theme.default.cssx">
+    <link rel="stylesheet" href="../assets/css/style.css" />
+    <link rel="stylesheet" href="../assets/css/responsive.css" />
+</head>
+
+<body>
+    <header>
+
+    </header>
+
+
+
+
+
+    <!-- video code  -->
+
+    <!-- <div class="video-section">
+            <video id="my-video" class="video-js vjs-big-play-centered" controls preload="auto" 
+                height="400" poster="assets/images/p-8.png" data-setup="{}">
+                <source src="assets/video/demo-video.mp4" type="video/mp4" />
+            </video>
+    </div> -->
+
+    <!-- video end -->
+
+
+
+
+
+
+
+    <!-- COUNTER html CODE  -->
+
+    <!-- SIX WRAP IS PARAENT CLASS TO TARGET IN CUSTOM JS  -->
+    <!-- <section class="six-wrap">
+        <h2><span class="counter counter1">175</span>+</h2>
+    </section> -->
+
+    <!-- COUNTER html CODE Close -->
+
+
+    <!-- fancy box  -->
+    <!-- <a data-fancybox="gallery" href="./images/portfolio/2.png">
+        <img src="./images/portfolio/2.png" class="img-fluid">
+    </a> -->
+    <!-- fancybox CLOSE  -->
---
+++ starter-volume-2/index.php
@@ -0,0 +1,8 @@
+<?php include './components/header.php'; ?>
+
+<section class="home-banner-main-wrap">
+    
+</section>
+
+
+<?php include './components/footer.php'; ?>
