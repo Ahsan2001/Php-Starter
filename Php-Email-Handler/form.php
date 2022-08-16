f93697d94ebe0b2f36cde979670de0bdbc177aa0
M. Ahsan Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2022-08-16 21:21:50 +0500

    Update form.php

--- Php-Email-Handler/form.php
+++ Php-Email-Handler/form.php
@@ -1,1 +1,39 @@
 
+	<div class="banform">
+				<div class="container">
+					<div class="row">
+						<div class="ban-form">
+							<form id="lpweb" method="POST" action="/sending/">
+								<div class="row">
+									<div class="col-lg-12">
+										<div class="fldset">
+											<input type="text" name="cn" placeholder="Enter Your Name" required="">
+										</div>
+									</div>
+									<div class="col-lg-12">
+										<div class="fldset">
+											<input type="email" name="em" placeholder="Enter email here" required>
+										</div>
+									</div>
+									<div class="col-lg-12">
+										<div class="fldset">
+											<input type="text" name="pn" placeholder="Enter Your Number" maxlength="11" minlength="5"	required="">
+										</div>
+									</div>
+									<div class="col-lg-12">
+										<div class="fldset">
+											<textarea name="msg" rows="7" placeholder="Talk About Your Project"
+												required></textarea>
+										</div>
+									</div>
+									<div class="col-lg-12">
+										<div class="fldset">
+											<input type="submit" placeholder="Connect With Us" />
+										</div>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
