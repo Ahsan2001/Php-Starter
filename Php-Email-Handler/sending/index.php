13fc5df88786dcf2659744c591b678aa248d9c55
M. Ahsan Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2022-08-16 21:18:41 +0500

    Update index.php

--- Php-Email-Handler/sending/index.php
+++ Php-Email-Handler/sending/index.php
@@ -1,1 +1,19 @@
-
+<?php
+$name = $_POST['cn'];
+$email = $_POST['em'];
+$phone = $_POST['pn'];
+$message = $_POST['msg'];
+$requirement = $_POST['requirement'];
+$needs = $_POST['needs'];
+$bussiness = $_POST['business_info']; 
+$industry = $_POST['industry']; 
+$price = $_POST['price']; 
+$actual_link = $_SERVER[HTTP_REFERER];
+// var_dump($actual_link);die;
+$formcontent="From: $name \n Email: $email  \n Phone: $phone  \n Message: $message \n Website Requirement: $requirement \n Website Needs: $needs  \n Website Bussines: $bussiness \n Industry: $industry \n Price: $price \n Url : $actual_link ";
+$recipient = "info@infinitywebstudios.com";
+$subject = "Website Lead Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+header("Location: https://infinitywebstudios.com/ThankYou ", true, 301);
+?>
