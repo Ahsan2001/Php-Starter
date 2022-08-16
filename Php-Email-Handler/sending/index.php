52a480378a2dcee3ca9f349c8864528cba604dcb
M. Ahsan Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2022-08-17 04:17:37 +0500

    Update index.php

--- Php-Email-Handler/sending/index.php
+++ Php-Email-Handler/sending/index.php
@@ -1,19 +1,15 @@
 <?php
-$name = $_POST['cn'];
-$email = $_POST['em'];
-$phone = $_POST['pn'];
-$message = $_POST['msg'];
-$requirement = $_POST['requirement'];
-$needs = $_POST['needs'];
-$bussiness = $_POST['business_info']; 
-$industry = $_POST['industry']; 
-$price = $_POST['price']; 
-$actual_link = $_SERVER[HTTP_REFERER];
-// var_dump($actual_link);die;
-$formcontent="From: $name \n Email: $email  \n Phone: $phone  \n Message: $message \n Website Requirement: $requirement \n Website Needs: $needs  \n Website Bussines: $bussiness \n Industry: $industry \n Price: $price \n Url : $actual_link ";
-$recipient = "info@infinitywebstudios.com";
-$subject = "Website Lead Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-header("Location: https://infinitywebstudios.com/ThankYou ", true, 301);
-?>
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $services = $_POST['services'];
+    $budget = $_POST['budget'];
+    $brief = $_POST['brief'];
+    $formcontent="From: $name \n Email: $email \n Phone: $phone \n Services: $services \n Budget: $budget \n Message: $brief";
+    $recipient = "ahsan.sabir@switchbytes.com";
+    $subject = "Website Lead Form";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "Thank You!";
+    header("Location: https://webdesignpreviews.com/html/thehouseofauthors", true, 301);
+?> 
