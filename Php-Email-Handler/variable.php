3ecacedb7ce05bd5e3a2ab08ded09de054bfd1dd
M. Ahsan Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2022-08-17 04:20:17 +0500

    Create variable.php

---
+++ Php-Email-Handler/variable.php
@@ -0,0 +1,25 @@
+<?php 
+$phone = "+02045715954";
+$email = "info@myacademics.co.uk ";
+$copyrights = "© myacademics.co.uk All Rights Reserved.";
+$logo = "img/logo.png";
+$add = "8 St James's Square, London, United Kingdom, SW1Y 4JU";
+?>
+
+
+
+   <div class="col-lg-7">
+        <a href="tel:<?php echo $phone; ?>">
+          <?php echo $phone; ?>
+        </a>
+    </div>
+    <div class="col-lg-5">
+        <a href="mailto:<?php echo $email; ?>">
+            <?php echo $email;?>
+        </a>
+    </div>
+
+
+
+
+
